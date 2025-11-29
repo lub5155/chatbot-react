0da0f221ec1830824e8f1489edb497708e8dcf1d
lub5155 <vtarun2004@gmail.com>
2025-11-30 00:50:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,39 +6,63 @@ function ChatInp(){
             </>
           )
         }
-        function ChatMsg(props){
-          const message=props.message
-          const sender=props.sender
-          return (
-            <>
-                {sender === "user" && (
-                <div id="user">
-                  {message}
-                  <img 
-                    src="https://imgs.search.brave.com/hTvTNBzxQhdVzNHWp0r5e48rro5EL6rHP2CPKKWLZJs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvbWFuLWF2YXRh/ci1wcm9maWxlLXBp/Y3R1cmUtdmVjdG9y/LWlsbHVzdHJhdGlv/bl8yNjg4MzQtNTM4/LmpwZz9zZW10PWFp/c19oeWJyaWQmdz03/NDAmcT04MA" 
-                    alt="profile pic" 
-                    width="50px" 
-                  />
-                </div>
-                )}
-                {sender === "bot" && (
-                <div id="bot">
-                  <img 
-                    src="https://img.lovepik.com/free-png/20211129/lovepik-robot-avatar-png-image_401191268_wh1200.png" 
-                    alt="profile pic" 
-                    width="50px" 
-                  />
-                  {message}
-                </div>
-                )}
-            </>
-           )
-        }
+function ChatMsg(props){
+  const message=props.message
+  const sender=props.sender
+      return (
+        <>
+           {sender === "user" && (
+            <div id="user">
+                {message}
+                <img 
+                  src="https://imgs.search.brave.com/hTvTNBzxQhdVzNHWp0r5e48rro5EL6rHP2CPKKWLZJs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvbWFuLWF2YXRh/ci1wcm9maWxlLXBp/Y3R1cmUtdmVjdG9y/LWlsbHVzdHJhdGlv/bl8yNjg4MzQtNTM4/LmpwZz9zZW10PWFp/c19oeWJyaWQmdz03/NDAmcT04MA" 
+                  alt="profile pic" 
+                  width="50px" 
+                />
+            </div>)}
+            {sender === "bot" && (
+              <div id="bot">
+                <img 
+                  src="https://img.lovepik.com/free-png/20211129/lovepik-robot-avatar-png-image_401191268_wh1200.png" 
+                  alt="profile pic" 
+                  width="50px" 
+                />
+                {message}
+              </div>)}
+        </>)
+}
+function ChatMessage(){
+  const messageComponents=[{
+    message:'hello bro',
+    sender:"user",
+    id:"id1"
+  },
+  {
+    message:'how can i help u?',
+    sender:"bot",
+    id:"id2"
+  },
+  {  message:'hello bro',
+    sender:"user",
+    id:"id3"
+  },
+  {
+    message:'how can i help u?',
+    sender:"bot",
+    id:"id4"
+  }]
+  return (
+    <>
+     {messageComponents.map((messageComponents) => {
+       <ChatMsg message={messageComponents.message} sender={messageComponents.sender}/>
+     })}
+    </>
+  )
+}
  function App(){
           return (
              <div >
               <div id="box">
-                  <ChatMsg message='hello bro' sender="user"/>
                   <ChatMsg message='how can i help u?' sender="bot"/>
                   <ChatMsg message='hello bro' sender="user"/>
                   <ChatMsg message='how can i help u?' sender="bot"/>
